f90d9ac97d908cfc1e5fc5b3382e1346eabdaaca
adriangonc <adriangonc@yahoo.com.br>
2023-05-22 14:08:45 -0300

    Insertion of H2 database configuration for tests

--- src/main/java/com/hexagonal/poc/infra/configuration/adapters/repositories/ProductRepository.java
+++ src/main/java/com/hexagonal/poc/infra/configuration/adapters/repositories/ProductRepository.java
@@ -2,15 +2,25 @@ package com.hexagonal.poc.infra.configuration.adapters.repositories;
 
 import com.hexagonal.poc.domain.Product;
 import com.hexagonal.poc.domain.ports.repositories.ProductRepositoryPort;
+import com.hexagonal.poc.infra.configuration.adapters.entities.ProductEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProductRepository implements ProductRepositoryPort {
+
+    private final SpringProductRepository springProductRepository;
+
+    public ProductRepository(SpringProductRepository springProductRepository) {
+        this.springProductRepository = springProductRepository;
+    }
+
     @Override
     public List<Product> findAll() {
-        return null;
+        List<ProductEntity> productEntities = this.springProductRepository.findAll();
+        return productEntities.stream().map(ProductEntity::toProduct).collect(Collectors.toList());
     }
 
     @Override
