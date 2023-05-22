dcc270ba82293d22b268bf7f8d124cf9614f83ea
adriangonc <adriangonc@yahoo.com.br>
2023-05-22 11:58:03 -0300

    Implementation of method to list all products and converto do product DTO.

--- src/main/java/com/hexagonal/poc/domain/adapters/services/OrderServiceImpl.java
+++ src/main/java/com/hexagonal/poc/domain/adapters/services/OrderServiceImpl.java
@@ -1,2 +1,36 @@
-package com.hexagonal.poc.domain.adapters.services;public class OrderServiceImpl {
+package com.hexagonal.poc.domain.adapters.services;
+
+import com.hexagonal.poc.domain.Product;
+import com.hexagonal.poc.domain.dto.ProductDTO;
+import com.hexagonal.poc.domain.dto.StockDTO;
+import com.hexagonal.poc.domain.ports.interfaces.ProductServicePort;
+import com.hexagonal.poc.domain.ports.repositories.ProductRepositoryPort;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderServiceImpl implements ProductServicePort {
+
+    private final ProductRepositoryPort productRepositoryPort;
+
+    public OrderServiceImpl(ProductRepositoryPort productRepositoryPort) {
+        this.productRepositoryPort = productRepositoryPort;
+    }
+
+    @Override
+    public List<ProductDTO> findAllProducts() {
+        List<Product> products = this.productRepositoryPort.findAll();
+        List<ProductDTO> productsDTO = products.stream().map(Product::toProductDto).collect(Collectors.toList());
+        return productsDTO;
+    }
+
+    @Override
+    public void createProduct(ProductDTO productDTO) {
+
+    }
+
+    @Override
+    public void updateStock(String sku, StockDTO stockDTO) {
+
+    }
 }
