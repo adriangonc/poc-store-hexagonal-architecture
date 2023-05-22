9ae0a1d7e9b1f4fbd1c830521cab837ab32c46ce
adriangonc <adriangonc@yahoo.com.br>
2023-05-22 14:50:32 -0300

    Add docker compose for project dependencies

---
+++ docker/init.sql
@@ -0,0 +1,11 @@
+-- create a table
+CREATE TABLE test(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  archived BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- add test data
+INSERT INTO test (name, archived)
+  VALUES ('test row 1532', true),
+  ('test row 2', false);
