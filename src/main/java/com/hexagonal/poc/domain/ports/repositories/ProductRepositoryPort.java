d802aa0e2dfd09b9a6c505043d90e0316fd14491
adriangonc <adriangonc@yahoo.com.br>
2023-05-22 11:50:31 -0300

    Creation of Product repository port

---
+++ src/main/java/com/hexagonal/poc/domain/ports/repositories/ProductRepositoryPort.java
@@ -0,0 +1,15 @@
+package com.hexagonal.poc.domain.ports.repositories;
+
+import com.hexagonal.poc.domain.Product;
+
+import java.util.List;
+
+public interface ProductRepositoryPort {
+
+    List<Product> findAll();
+
+    Product findBySku(String sku);
+
+    void save(Product product);
+
+}
